edafdf8d80b36ed1c9634726a2c42675d2291cc7
Doğaç Mengül <dogacmengul@yahoo.com>
2024-09-30 17:49:11 +0200

    minor fixes

--- Assets/Scripts/Editors/EnemyEditorWindow.cs
+++ Assets/Scripts/Editors/EnemyEditorWindow.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 using UnityEditor;
 
+// editor for creating enemy details SOs
 public class EnemyEditorWindow : EditorWindow
 {
-    // editor for creating enemy details SOs
     private string _enemyType;
     private string _assetPath = "Assets/ScriptableObjects/Enemies/";
-    private EnemyDetailsSO _enemyDetailsInstance;
-    private EnemyDetailsSO _selectedEnemyDetailsInstance;
+    private EnemyDetailsSO _newEnemyDetails;
+    private EnemyDetailsSO _selectedEnemyDetails;
     private SerializedObject _serializedObject;
     private SerializedProperty _typeProp;
     private SerializedProperty _enemyPrefabProp;
@@ -32,7 +32,11 @@ public class EnemyEditorWindow : EditorWindow
 
     private void OnGUI()
     {
-        GUILayout.Label("Create Enemy Scriptable Object", EditorStyles.boldLabel);
+        GUILayout.Space(20);
+
+        // create enemy section
+
+        GUILayout.Label("Create Enemy Details Scriptable Object", EditorStyles.boldLabel);
 
         _enemyType = EditorGUILayout.TextField("Type", _enemyType);
 
@@ -41,24 +45,11 @@ public class EnemyEditorWindow : EditorWindow
             CreateScriptableObject();
         }
 
-        if (_enemyDetailsInstance != null)
+        if (_newEnemyDetails != null)
         {
             GUILayout.Label("New Fields", EditorStyles.boldLabel);
 
-            _enemyDetailsInstance.Type = _enemyType;
-            _enemyDetailsInstance.EnemyPrefab = (GameObject)EditorGUILayout.ObjectField(
-                "EnemyPrefab", _enemyDetailsInstance.EnemyPrefab, typeof(GameObject), false);
-            _enemyDetailsInstance.SpeedMin = EditorGUILayout.FloatField("SpeedMin", _enemyDetailsInstance.SpeedMin);
-            _enemyDetailsInstance.SpeedMax = EditorGUILayout.FloatField("SpeedMax", _enemyDetailsInstance.SpeedMax);
-            _enemyDetailsInstance.DodgeThrust = EditorGUILayout.FloatField("DodgeThrust", _enemyDetailsInstance.DodgeThrust);
-            _enemyDetailsInstance.HealthMin = EditorGUILayout.IntField("HealthMin", _enemyDetailsInstance.HealthMin);
-            _enemyDetailsInstance.HealthMax = EditorGUILayout.IntField("HealthMax", _enemyDetailsInstance.HealthMax);
-            _enemyDetailsInstance.Damage = EditorGUILayout.FloatField("Damage", _enemyDetailsInstance.Damage);
-            _enemyDetailsInstance.ExperienceDrop = EditorGUILayout.IntSlider("ExperienceDrop", _enemyDetailsInstance.ExperienceDrop, 1, 20);
-            _enemyDetailsInstance.Awareness = EditorGUILayout.Slider("Awareness", _enemyDetailsInstance.Awareness, 0, 2);
-            _enemyDetailsInstance.Agility = EditorGUILayout.Slider("Agility", _enemyDetailsInstance.Agility, 0, 20);
-            _enemyDetailsInstance.EnemyDeathEffect = (EnemyDeathEffectSO)EditorGUILayout.ObjectField(
-                "EnemyDeathEffect", _enemyDetailsInstance.EnemyDeathEffect, typeof(EnemyDeathEffectSO), false);
+            InitializeScriptableObject();
 
             if (GUILayout.Button("Save Enemy Details Scriptable Object"))
             {
@@ -66,50 +57,18 @@ public class EnemyEditorWindow : EditorWindow
             }
         }
 
-        GUILayout.Space(10);
+        GUILayout.Space(20);
+
+        // edit enemy section
+
         GUILayout.Label("Edit Enemy Details Scriptable Object", EditorStyles.boldLabel);
 
-        _selectedEnemyDetailsInstance = (EnemyDetailsSO)EditorGUILayout.ObjectField(
-            "Select Scriptable Object", _selectedEnemyDetailsInstance, typeof(EnemyDetailsSO), false);
+        _selectedEnemyDetails = (EnemyDetailsSO)EditorGUILayout.ObjectField(
+            "Select Scriptable Object", _selectedEnemyDetails, typeof(EnemyDetailsSO), false);
 
-        if (_selectedEnemyDetailsInstance != null)
+        if (_selectedEnemyDetails != null)
         {
-            if (_serializedObject == null || _serializedObject.targetObject != _selectedEnemyDetailsInstance)
-            {
-                _enemyDetailsInstance = null; // to clear the create section
-
-                _serializedObject = new SerializedObject(_selectedEnemyDetailsInstance);
-
-                _typeProp = _serializedObject.FindProperty("Type");
-                _enemyPrefabProp = _serializedObject.FindProperty("EnemyPrefab");
-                _speedMinProp = _serializedObject.FindProperty("SpeedMin");
-                _speedMaxProp = _serializedObject.FindProperty("SpeedMax");
-                _dodgeThrustProp = _serializedObject.FindProperty("DodgeThrust");
-                _healthMinProp = _serializedObject.FindProperty("HealthMin");
-                _healthMaxProp = _serializedObject.FindProperty("HealthMax");
-                _damageProp = _serializedObject.FindProperty("Damage");
-                _experienceDropProp = _serializedObject.FindProperty("ExperienceDrop");
-                _awarenessProp = _serializedObject.FindProperty("Awareness");
-                _agilityProp = _serializedObject.FindProperty("Agility");
-                _enemyDeathEffect = _serializedObject.FindProperty("EnemyDeathEffect");
-            }
-
-            _serializedObject.Update();
-
-            EditorGUILayout.PropertyField(_typeProp);
-            EditorGUILayout.PropertyField(_enemyPrefabProp);
-            EditorGUILayout.PropertyField(_speedMinProp);
-            EditorGUILayout.PropertyField(_speedMaxProp);
-            EditorGUILayout.PropertyField(_dodgeThrustProp);
-            EditorGUILayout.PropertyField(_healthMinProp);
-            EditorGUILayout.PropertyField(_healthMaxProp);
-            EditorGUILayout.PropertyField (_damageProp);
-            EditorGUILayout.PropertyField(_experienceDropProp);
-            EditorGUILayout.PropertyField(_awarenessProp);
-            EditorGUILayout.PropertyField(_agilityProp);
-            EditorGUILayout.PropertyField(_enemyDeathEffect);
-
-            _serializedObject.ApplyModifiedProperties();
+            EditScriptableObject();
 
             if (GUILayout.Button("Save Enemy Details Scriptable Object"))
             {
@@ -120,29 +79,92 @@ public class EnemyEditorWindow : EditorWindow
 
     private void CreateScriptableObject()
     {
-        _enemyDetailsInstance = CreateInstance<EnemyDetailsSO>();
+        _newEnemyDetails = CreateInstance<EnemyDetailsSO>();
+    }
+
+    private void InitializeScriptableObject()
+    {
+        _newEnemyDetails.Type = _enemyType;
+        _newEnemyDetails.EnemyPrefab = (GameObject)EditorGUILayout.ObjectField(
+            "EnemyPrefab", _newEnemyDetails.EnemyPrefab, typeof(GameObject), false);
+        _newEnemyDetails.SpeedMin = EditorGUILayout.FloatField("SpeedMin", _newEnemyDetails.SpeedMin);
+        _newEnemyDetails.SpeedMax = EditorGUILayout.FloatField("SpeedMax", _newEnemyDetails.SpeedMax);
+        _newEnemyDetails.DodgeThrust = EditorGUILayout.FloatField("DodgeThrust", _newEnemyDetails.DodgeThrust);
+        _newEnemyDetails.HealthMin = EditorGUILayout.IntField("HealthMin", _newEnemyDetails.HealthMin);
+        _newEnemyDetails.HealthMax = EditorGUILayout.IntField("HealthMax", _newEnemyDetails.HealthMax);
+        _newEnemyDetails.Damage = EditorGUILayout.FloatField("Damage", _newEnemyDetails.Damage);
+        _newEnemyDetails.ExperienceDrop = EditorGUILayout.IntSlider("ExperienceDrop", _newEnemyDetails.ExperienceDrop, 1, 20);
+        _newEnemyDetails.Awareness = EditorGUILayout.Slider("Awareness", _newEnemyDetails.Awareness, 0, 2);
+        _newEnemyDetails.Agility = EditorGUILayout.Slider("Agility", _newEnemyDetails.Agility, 0, 20);
+        _newEnemyDetails.EnemyDeathEffect = (EnemyDeathEffectSO)EditorGUILayout.ObjectField(
+            "EnemyDeathEffect", _newEnemyDetails.EnemyDeathEffect, typeof(EnemyDeathEffectSO), false);
+    }
+
+    private void EditScriptableObject()
+    {
+        if (_serializedObject == null || _serializedObject.targetObject != _selectedEnemyDetails)
+        {
+            _newEnemyDetails = null; // to clear the create section
+
+            _serializedObject = new SerializedObject(_selectedEnemyDetails);
+
+            // get values from SO
+
+            _typeProp = _serializedObject.FindProperty("Type");
+            _enemyPrefabProp = _serializedObject.FindProperty("EnemyPrefab");
+            _speedMinProp = _serializedObject.FindProperty("SpeedMin");
+            _speedMaxProp = _serializedObject.FindProperty("SpeedMax");
+            _dodgeThrustProp = _serializedObject.FindProperty("DodgeThrust");
+            _healthMinProp = _serializedObject.FindProperty("HealthMin");
+            _healthMaxProp = _serializedObject.FindProperty("HealthMax");
+            _damageProp = _serializedObject.FindProperty("Damage");
+            _experienceDropProp = _serializedObject.FindProperty("ExperienceDrop");
+            _awarenessProp = _serializedObject.FindProperty("Awareness");
+            _agilityProp = _serializedObject.FindProperty("Agility");
+            _enemyDeathEffect = _serializedObject.FindProperty("EnemyDeathEffect");
+        }
+
+        _serializedObject.Update();
+
+        EditorGUILayout.PropertyField(_typeProp);
+        EditorGUILayout.PropertyField(_enemyPrefabProp);
+        EditorGUILayout.PropertyField(_speedMinProp);
+        EditorGUILayout.PropertyField(_speedMaxProp);
+        EditorGUILayout.PropertyField(_dodgeThrustProp);
+        EditorGUILayout.PropertyField(_healthMinProp);
+        EditorGUILayout.PropertyField(_healthMaxProp);
+        EditorGUILayout.PropertyField(_damageProp);
+        EditorGUILayout.PropertyField(_experienceDropProp);
+        EditorGUILayout.PropertyField(_awarenessProp);
+        EditorGUILayout.PropertyField(_agilityProp);
+        EditorGUILayout.PropertyField(_enemyDeathEffect);
+
+        _serializedObject.ApplyModifiedProperties();
     }
 
     private void SaveScriptableObject()
     {
-        if (_enemyDetailsInstance != null)
+        if (_newEnemyDetails != null)
         {
             string path = AssetDatabase.GenerateUniqueAssetPath(_assetPath + "EnemyDetails_" + _enemyType + ".asset");
-            AssetDatabase.CreateAsset(_enemyDetailsInstance, path);
+            AssetDatabase.CreateAsset(_newEnemyDetails, path);
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
             EditorUtility.FocusProjectWindow();
-            Selection.activeObject = _enemyDetailsInstance;
+            Selection.activeObject = _newEnemyDetails;
         }
     }
 
     private void SaveEditedScriptableObject()
     {
-        EditorUtility.SetDirty(_selectedEnemyDetailsInstance);
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-        EditorUtility.FocusProjectWindow();
-        Selection.activeObject = _selectedEnemyDetailsInstance;
+        if (_selectedEnemyDetails != null)
+        {
+            EditorUtility.SetDirty(_selectedEnemyDetails);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = _selectedEnemyDetails;
+        }
     }
 
 }
